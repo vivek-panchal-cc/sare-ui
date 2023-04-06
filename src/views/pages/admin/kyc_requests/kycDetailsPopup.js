1144488ab74a2539e00a29cbde24250f3a1521e8
Vivek Panchal <Vivek.p@cctdigisol.local>
2023-04-06 18:27:19 +0530

    Admin Panel Changes Done

---
+++ src/views/pages/admin/kyc_requests/kycDetailsPopup.js
@@ -0,0 +1,36 @@
+import React from "react";
+import {
+  CModal,
+  CModalHeader,
+  CModalTitle,
+  CModalBody,
+  CModalFooter,
+  CButton,
+} from "@coreui/react";
+
+//  To display only readable details when status is rejected or approved. (Created by Vivek Panchal)
+
+const KYCDetailsPopup = ({ kycDetail, onClose }) => {
+  return (
+    <CModal show={true} onClose={onClose}>
+      <CModalHeader closeButton>
+        <CModalTitle>KYC Details</CModalTitle>
+      </CModalHeader>
+      <CModalBody>
+        <p>
+          Status:{" "}
+          {kycDetail.status.charAt(0).toUpperCase() + kycDetail.status.slice(1)}
+        </p>
+        <p>Comment: {kycDetail.admin_comment}</p>
+        {/* Add more fields here as needed */}
+      </CModalBody>
+      <CModalFooter>
+        <CButton color="secondary" onClick={onClose}>
+          Close
+        </CButton>
+      </CModalFooter>
+    </CModal>
+  );
+};
+
+export default KYCDetailsPopup;
